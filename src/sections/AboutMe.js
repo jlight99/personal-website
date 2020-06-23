fde2f132f05597667beeb6db5761af657e31ed00
jlight99 <ellenhuang523@gmail.com>
2020-06-23 10:54:13 -0400

    edit spacing of about page

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -8,16 +8,18 @@ class AboutMe extends React.Component {
         return (
             <a id="about_me">
                 <span className="about">
-                    <span id="picture-of-me">
-                        <img src="/resources/IMG_0272.JPG"></img>
-                    </span>
-                    <span className="about-section">
-                        <div className="hi">hey, i'm ellen!</div>
-                        <div className="about-text">
-                            i study software engineering at the university of waterloo.
-                            in my free time, i enjoy learning new languages, running, and playing music.
+                    <span className="about-inner">
+                        <span id="picture-of-me">
+                            <img src="/resources/IMG_0272.JPG"></img>
+                        </span>
+                        <span className="about-section">
+                            <div className="hi">hey, i'm ellen!</div>
+                            <div className="about-text">
+                                i study software engineering at the university of waterloo.
+                                in my free time, i enjoy learning new languages, running, and playing music.
                         </div>
-                        <span className="contact-info"><ContactInfo /></span>
+                            <span className="contact-info"><ContactInfo /></span>
+                        </span>
                     </span>
                 </span>
             </a>
