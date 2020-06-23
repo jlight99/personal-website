cc9cef87d0233c14e89ef11cc2e9e6d868ae4833
jlight99 <ellenhuang523@gmail.com>
2020-06-23 10:37:40 -0400

    Updates

---
+++ precache-manifest.fde77a950f37960b3754b48a6f950068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e11e55dd19ee16ffefaa30786dff86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ed20194bd5f89976e3",
+    "url": "/static/css/main.4cef5d46.chunk.css"
+  },
+  {
+    "revision": "ec5b1dabb41fd0d104bc",
+    "url": "/static/js/2.b6d6f131.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.b6d6f131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ed20194bd5f89976e3",
+    "url": "/static/js/main.546ba013.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
